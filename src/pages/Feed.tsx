08a07a74e948119a897d38f9c497be33c9e386d5
Gustavo <opereiragu@gmail.com>
2021-07-19 22:23:52 -0300

    tets

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,50 +1,40 @@
 import React from 'react';
-import {
-    View, 
-    Text, 
-    StyleSheet
-    
-    
-} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
-export function Feed (){
-
+export function Feed(){
     return(
- 
-        <View style={styles.container}> 
-
-        <View><Text style={styles.title}> News </Text></View>
-
-        <View><Text style={styles.news}>  </Text></View>
-
+        <View style={style.title}>
+        <View style={style.news}><Text>News</Text></View>
+        <View style={style.buttons}><Text>Inicio Menu Perfil </Text></View>
         </View>
-        
     )
 }
 
-const styles = StyleSheet.create({
-
-    container: {
-        width: '100%',
-        
-        paddingVertical: 60,
-        backgroundColor: colors.gray
-        
-    },
-
+const style = StyleSheet.create({
     title: {
-        fontSize: 60,
-        alignItems:'center',
-        fontFamily: fonts.heading
-
+        fontSize: 75,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 40,
+        fontFamily: fonts.heading,
+        backgroundColor: colors.gray
     },
 
-    news: {   
-        
-        
+    news: {
+        justifyContent: 'center',
+        paddingVertical: 75,
+        backgroundColor: colors.gray
+    },
 
+    buttons: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 75,
+        backgroundColor: colors.gray
     },
 
 })
