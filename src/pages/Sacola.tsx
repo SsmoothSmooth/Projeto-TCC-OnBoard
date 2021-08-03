f0c7fe5235ed80ce2cb47885db1f9a50e19a4b26
José Wilson de Lima <josewilson791@gmail.com>
2021-08-03 20:10:43 -0300

    Crianaçã da Interface Sacola

---
+++ src/pages/Sacola.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import { useNavigation } from '@react-navigation/core';
+
+export function Sacola(){
+
+
+    return(
+        <SafeAreaView > 
+             
+        </SafeAreaView> 
+    )
+}
+
