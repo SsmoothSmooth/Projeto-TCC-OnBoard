9df26a84e2e9da9d53fcae9a031b033da49a08a4
José Wilson de Lima <josewilson791@gmail.com>
2021-09-26 12:17:15 -0300

    Componenete  do header

---
+++ src/components/ModHeader.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import{
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+interface TextHeader {
+    photo?: string;
+    title: string;
+    photo2? : string;
+
+}
+
+export function ModHeader({ title, ...rest}: TextHeader) {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.title}>
+                    { title }
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        width: '100%',
+        alignItems: 'center',
+        marginTop: getStatusBarHeight(),
+    },
+
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingHorizontal: '10%',
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        fontSize: 40,
+    },
+
+});
